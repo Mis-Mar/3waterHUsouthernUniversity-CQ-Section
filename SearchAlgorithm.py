d641e738c20c2586f057689698139795268ee81f
Teshdenis Inhovin <13088086708@163.com>
2025-06-25 19:46:38 +0800

    完成寻路算法前半部分

--- SearchAlgorithm.py
+++ SearchAlgorithm.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
+from queue import PriorityQueue
+
 
 
 class Node:
@@ -93,6 +95,59 @@ class Algorithm:
             self.bfs_distance(self.board.grid[x][y])
         self.calculate_influence()
 
+    def reverse_bfs(self, start_node, demand_threshold):
+        """
+        反向BFS搜索，返回满足需求值的源点列表
+        """
+        open_list = PriorityQueue()  # 大根堆，存储 (负的最终影响值, 节点)
+        close_list = set()  # 已访问节点集合
+        source_points = []  # 源点列表
+        accumulated_value = 0  # 累计价值
+
+        # 初始化：将起始节点加入open表
+        open_list.put((-start_node.final_influence, start_node))
+        while not open_list.empty():
+            _, current = open_list.get()  # 取出堆顶节点
+            if (current.x, current.y) in close_list:
+                continue
+            close_list.add((current.x, current.y))
+
+            # 累计价值
+            if current.value > 0:  # 如果是价值点
+                accumulated_value += current.value
+                source_points.append((current.x, current.y, current.value))  # 记录源点
+
+            # 如果累计价值超过需求值，则终止搜索
+            if accumulated_value >= demand_threshold:
+                break
+
+            # 扩展邻居节点
+            for neighbor in self.board.get_neighbors(current):
+                if (neighbor.x, neighbor.y) not in close_list:
+                    open_list.put((-neighbor.final_influence, neighbor))
+
+        return source_points
+
+    def forward_bfs_algorithm(self, target_points, demand_threshold):
+        """
+        主函数：整合前向BFS和反向BFS
+        :param target_points: 目标点列表 [(x, y, 需求值), ...]
+        :param demand_threshold: 需求值阈值
+        :return: 源点列表 [(x, y, 当前节点价值), ...]
+        """
+        # 前向BFS：计算所有节点的最终影响值
+        self.compute_final_influences(target_points)
+
+        # 反向BFS：从目标点出发获取源点表
+        source_points = []
+        for x, y, demand in target_points:
+            start_node = self.board.grid[x][y]
+            source_points.extend(self.reverse_bfs(start_node, demand))
+
+        return source_points
+
+
+
 
 class Visualizer:
     def __init__(self, board):
@@ -142,7 +197,8 @@ if __name__ == "__main__":
     board.set_target_points(target_points)
 
     algorithm = Algorithm(board, tunable_param=3, threshold=1)
-    algorithm.compute_final_influences(target_points)
+    source_points = algorithm.forward_bfs_algorithm(target_points, demand_threshold=8)
+    print("源点表:", source_points)
 
     visualizer = Visualizer(board)
     visualizer.plot_board(target_points, value_points)
